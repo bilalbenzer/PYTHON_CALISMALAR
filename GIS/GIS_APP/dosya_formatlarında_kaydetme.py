78822bba44095219932bc2f691372b58b84fd029
Bilal Benzer <bllbnzr@gmail.com>
2022-02-24 21:27:05 +0300

    txt oluşturma güncellemesi

--- GIS/GIS_APP/dosya_formatlarında_kaydetme.py
+++ GIS/GIS_APP/dosya_formatlarında_kaydetme.py
@@ -51,48 +51,42 @@ def shape_olustur3d(args,tip):
             veritabani.to_file(konum) 
     else:
         bos_veri_tabanı = 0
-def text_olusturma2d(args):
+def text_olusturma2d(args,tip):
     for i in range(1):
         try:
             konum=input("Dosyayı kaydetmek istediğiniz konumu giriniz. Örnek:C:/Users/.../name.txt\nDosya Yolu:")
-            konum = r"{}".format(konum)
-            with open(konum,"w+") as dosya:
-                for i in args:
-                    print(i)
-                    dosya.write(str(i)+"\n")              
+            konum = r"{}".format(konum)             
         except :
                 print("Konum Bilgisini Yanlış Girdiniz. Lütfen Tekrar Deneyiniz.")
                 try:
                     konum=input("Dosyayı kaydetmek istediğiniz konumu giriniz. Örnek:C:/Users/.../name.txt\nDosya Yolu:")
-                    konum = r"{}".format(konum)
-                    with open(konum,"w+") as dosya:
-                        for i in args:
-                            print(i)
-                            dosya.write(str(i)+"\n")              
+                    konum = r"{}".format(konum)          
                 except :
                         print("Konum Bilgisini Yanlış Girdiniz. Program Sonlanmıştır.")
                         break
-def text_olusturma3d(args):
+        if tip =="nokta":
+            verii=f"""AD-TIP-X-Y-GEOMETRY
+{args.point_ad},{args.geometri_tip},{args.x_koordinati},{args.y_koordinati},{args.geometri}"""
+            with open(konum,"w+") as dosya:
+                dosya.write(str(verii)+"\n")
+def text_olusturma3d(args,tip):
     for i in range(1):
         try:
             konum=input("Dosyayı kaydetmek istediğiniz konumu giriniz. Örnek:C:/Users/.../name.txt\nDosya Yolu:")
-            konum = r"{}".format(konum)
-            with open(konum,"w+") as dosya:
-                for i in args:
-                    print(i)
-                    dosya.write(str(i)+"\n")              
+            konum = r"{}".format(konum)             
         except :
                 print("Konum Bilgisini Yanlış Girdiniz. Lütfen Tekrar Deneyiniz.")
                 try:
                     konum=input("Dosyayı kaydetmek istediğiniz konumu giriniz. Örnek:C:/Users/.../name.txt\nDosya Yolu:")
-                    konum = r"{}".format(konum)
-                    with open(konum,"w+") as dosya:
-                        for i in args:
-                            print(i)
-                            dosya.write(str(i)+"\n")              
+                    konum = r"{}".format(konum)          
                 except :
                         print("Konum Bilgisini Yanlış Girdiniz. Program Sonlanmıştır.")
                         break
+        if tip =="nokta":
+            verii=f"""AD-TIP-X-Y-Z-GEOMETRY
+{args.point_ad},{args.geometri_tip},{args.x_koordinati},{args.y_koordinati},{args.z_koordinati},{args.geometri}"""
+            with open(konum,"w+") as dosya:
+                dosya.write(str(verii)+"\n")
 def xml_olusturma2d(args):
     for i in range(1):
         try:
