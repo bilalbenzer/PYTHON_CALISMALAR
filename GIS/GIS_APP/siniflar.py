43d40cd782ef385e2e908fcc8f4c4eb33f7f5248
Bilal Benzer <bllbnzr@gmail.com>
2022-02-26 16:34:22 +0300

    siniflarda güncelleme yapıldı (try,except)

--- GIS/GIS_APP/siniflar.py
+++ GIS/GIS_APP/siniflar.py
@@ -21,42 +21,40 @@ class point3d():
     geometry_type="Point Z"  #geometri tipi için sınıfa ait bir özellik
     def __init__(self,point_name=str(),x_koordinati=float(),y_koordinati=float(),z_koordinati=float(),crs_kod_epsg=float()):    #gelen değişkene atamak için örnek nitelikleri
         self.name = str(point_name)  #gelen değişkenin ismi
-        try:      
+        try:      #x koordinatı sayısal bir değer olmalı. bunun için try except yapıyoruz
             self.x = float(x_koordinati)   #gelen değişkenin x koordinatı
-        except ValueError:
+        except ValueError:  #kullanıcı, str bir değer verirse eğer, uyarı verilecek ve tekrar girmesi istenecek
             print("X Koordinatı yanlış girildi. Değeri tekrar atamnız gerekmektedir.")
-            self.x = None   #gelen değişkenin x koordinatı
-        try:
+            self.x = None   #hatalı girişten dolayı x koordinatına none değeri atıyoruz
+        try:    #y koordinatı sayısal bir değer olmalı. bunun için try except yapıyoruz
             self.y = float(y_koordinati)     #gelen değişkenin y koordinatı
-        except ValueError:
+        except ValueError:   #kullanıcı, str bir değer verirse eğer, uyarı verilecek ve tekrar girmesi istenecek
             print("Y Koordinatı yanlış girildi. Değeri tekrar atamnız gerekmektedir.")
-            self.y = None     #gelen değişkenin y koordinatı
-        try:            
+            self.y = None     #hatalı girişten dolayı y koordinatına none değeri atıyoruz
+        try:    #z koordinatı sayısal bir değer olmalı. bunun için try except yapıyoruz        
             self.z = float(z_koordinati)   #gelen değişkenin z koordinatı
-        except ValueError:
+        except ValueError:  #kullanıcı, str bir değer verirse eğer, uyarı verilecek ve tekrar girmesi istenecek
             print("Z Koordinatı yanlış girildi. Değeri tekrar atamnız gerekmektedir.")
-            self.z = None   #gelen değişkenin z koordinatı
-        try:
+            self.z = None   #hatalı girişten dolayı z koordinatına none değeri atıyoruz
+        try:    #koordinat sistemi epsg kodu sayısal bir değer olmalı. bunun için try except yapıyoruz
             self.crs_system = float(crs_kod_epsg)  #gelen değşikenin epsg kodu
-        except ValueError:
+        except ValueError:  #koordinat bilgisi, sayısal olmayan bir değer gelirse none değeri verilecek
             print("Koordinat sistemi EPSG kodu yanlış girildi. Değeri tekrar atamanız gerekmektedir.")
-            self.crs_system = None  #gelen değşikenin epsg kodu
-        try:
-            if (self.x == None) or (self.y == None):
+            self.crs_system = None  #hatalı girişten dolayı koordinat sistemi none olarak değişecek
+        # geometri objesi, x,y ve z değerlerinden oluşur. Bunlardan herhangi birinin hatalı girilemsi durumunda point objesi oluşamaz. Bunun için try except bloğu kullanacağız
+        try:   
+            if (self.x == None) or (self.y == None):    #point objesi temelde x ve y den oluşur. z değeri isteğe bağlıdır. x veya yden birisi yanlış verilirse eğer obje none olarak değişr
                 self.geometry = None
-            else:
-                if self.z == None:
+            else:   #x ve y doğru ise bu sefer z değerinin sorgusu yapılır
+                if self.z == None:  #z değeri yanlış girilmiş ve none değerini almış ise, geometri objesine z değeri dahil edilmez.
                     self.geometry = Point(self.x,self.y)    #gelen değişkenin geometri objesi
-                elif self.z ==float():
+                elif self.z ==float():  #z değerinde sorun yoksa eğer geometri objesine dahil edilir.
                     self.geometry = Point(self.x,self.y,self.z)
-        except ValueError:
+        except :    #herhangi bir hata olması durumunda geomtry objesi none değere dönüşür.
             self.geometry = None    #gelen değişkenin geometri objesi   
             return
     def oznitelik_ekleme(self,oznitelik=dict()):   #geometriye ait öznitelik bilgileri bu kısımda saklanabilir
-        self.attibuties = {"AD":self.name,"TIP":"Point","X":self.x,"Y":self.y,"Z":self.z}    #ilk olarak dict tipinde bir değişkenimiz olmalı
+        self.attibuties = {"AD":self.name,"TIP":"Point","X":self.x,"Y":self.y,"Z":self.z}    #ilk olarak dict tipinde bir değişkenimiz olmalı. bu sözlüğe varsayılan kolonları ekliyoruz.
         for i,c in oznitelik.items():   #kullanıcının verdiği bilgileri for döngüsü ile dict verimizie veriyoruz.
             self.attibuties.update({i:c})
 
-a = point3d()
-a.oznitelik_ekleme()
-print(a.attibuties)
