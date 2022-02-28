249ceb87dc06e9b3564b27d64a74a6c8dd69f977
Bilal Benzer <bllbnzr@gmail.com>
2022-03-01 01:18:34 +0300

    sesli tanımada geliştirmeler..

--- GIS/GIS_APP/GIS_APP_BETA.py
+++ GIS/GIS_APP/GIS_APP_BETA.py
@@ -6,6 +6,7 @@ cikis_mesaj = """
 -----------------------------------------------------------
 --- GIS UYGULAMAMIZI KULLANDIĞINIZ İÇİN TEŞEKKÜR EDERİZ ---
 -----------------------------------------------------------"""
+
 islem_turlerı = """
 1- GEOMETRİK NESNE OLUŞTURMA
     1.1- NOKTA OLUŞTURMA
@@ -20,17 +21,33 @@ islem_turlerı = """
 """
 print(giris_messaj)
 print(islem_turlerı)
+
 try:
     while True:
-        secim = input("Lütfen bir işlem türü seçiniz.Çıkış yapmak için 0 diyebilirsiniz.\nCevap:")
-        if secim =="1.4-":
-            from coklu_nokta_olusturma import nokta_olustur
-            nokta_olustur()
-        elif secim=="1.1-":
-            from nokta_olusturma import nokta_olustur
-            nokta_olustur()
-        elif secim=="0":
-            break
+        sesli_komut = input("Sesli Komut Kullanmak İstiyor Musunuz?\nEvet:1\nHayır:2\nCevap:")
+        print("Lütfen bir işlem türü seçiniz.Çıkış yapmak için 0 diyebilirsiniz.\nCevap:")
+        if sesli_komut=="2":
+            secim =input(":")         
+            if secim =="1.4-":
+                from coklu_nokta_olusturma import nokta_olustur
+                nokta_olustur()
+            elif secim=="1.1-":
+                from nokta_olusturma import nokta_olustur
+                nokta_olustur()
+            elif secim=="0":
+                break
+        elif sesli_komut=="1":
+            from konusma_tanima import kayit
+            text1=0
+            kayit()
+            print(text1)
+            secenekler = ["1.1","1.1-","Nokta Oluşturma","Nokta Oluşturmak İstiyorum","Nokta Oluştur"]
+            if text1 in secenekler:
+                from nokta_olusturma import nokta_olustur
+                nokta_olustur()
+            else:
+                print("DUrdu")
+                
 except KeyboardInterrupt as hata:
     print(hata,"\nİşlem iptal edilmiştir.\n",cikis_mesaj)
     
