b6b999d7c38b9805b05e25e48eea04808dd54ee0
Bilal Benzer <bllbnzr@gmail.com>
2022-03-06 22:38:49 +0300

    ..

--- WEB_GIS/WEB_GIS/WEB_GIS/settings.py
+++ WEB_GIS/WEB_GIS/WEB_GIS/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'WEB_GIS',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,9 @@ WSGI_APPLICATION = 'WEB_GIS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'geodjango',
+        'USER': 'geo',
     }
 }
 
