0be4a4955af52dac693e8ac0c4724b8386db1310
Bilal Benzer <bllbnzr@gmail.com>
2022-03-08 18:28:38 +0300

    webgis devam

--- WEB_GIS/WEB_GIS/urls.py
+++ WEB_GIS/WEB_GIS/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path("home_page/", include("home_page.urls")),
+    path("",TemplateView.as_view(template_name="index.html"),
+    ]
