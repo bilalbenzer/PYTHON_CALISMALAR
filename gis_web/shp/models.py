dcd44462b2ba94df525efb34c3686d4c0f27d650
Bilal Benzer <bllbnzr@gmail.com>
2022-03-14 12:11:26 +0300

    ..

--- gis_web/shp/models.py
+++ gis_web/shp/models.py
@@ -10,9 +10,10 @@ from geoalchemy2 import Geometry, WKTElement
 import glob
 import osgeo
 from geo.Geoserver import Geoserver
+from geo.Postgres import Db
 #initializing the library
 geo = Geoserver('http://127.0.0.1:8080/geoserver', username='admin', password='geoserver')
-
+db = Db(dbname="geoapp",user="postgres",password="gazi1453",host="localhost",port="5432")
 
 # Create your models here.
 #shapefile model
@@ -31,6 +32,7 @@ def publish_data(sender,instance,created,**kwargs):
     file_format = os.path.basename(file).split('.')[-1]
     file_name = os.path.basename(file).split('.')[0]
     file_path = os.path.dirname(file)
+    name = instance.name
     conn_str= "postgresql://postgres:gazi1453@localhost:5432/geoapp"
     print(file,'file')
     print(file_name)
@@ -41,35 +43,34 @@ def publish_data(sender,instance,created,**kwargs):
     with zipfile.ZipFile(file,"r") as zip_ref:
         zip_ref.extractall(file_path)
     os.remove(file) #remove zip file
-    shp = glob.glob(r"{}/**/*.shp".format(file_path),recursive=True)[0]    #to (get shp
+    shp = glob.glob(r"{}/**/*.shp".format(file_path),recursive=True)    #to (get shp
     print("-------------------------------------")
     print(shp,"shp")
     print("--------------------------------------")
-    gdf = gpd.read_file(shp)  #make geodataframe
-    crs_name = str(gdf.crs.srs)
-    print(crs_name,'crs_name')
-    epsg = 4326
-    if epsg is None:
-        epsg = 4326 #wgs84
-    geom_type = gdf.geom_type[1]
+    req_shp = shp[0]
+    print(req_shp)
+    gdf = gpd.read_file(req_shp)  #make geodataframe
+    gdf.head()
     engine = create_engine(conn_str)    #create sql alchemy engine to use 
-    gdf['gemom'] = gdf['geometry'].apply(lambda x:WKTElement(x.wkt,srid=epsg))
-    gdf.drop('geometry',1,inplace=True)    #drop the geometry column (since we already backup this column with geom)
-    gdf.to_sql(file_name,engine,'data',
-            if_exists='append',
-            index=False,dtype={'gemom' : Geometry('Geometry')})   #post gdf to postgresql
+
+    gdf.to_postgis(name,engine,"public",
+            if_exists='replace',
+            index=False)   #post gdf to postgresql
+    for s in (shp):
+        os.remove(s)
     
     '''
     Publish Shp To  Geoserver Using Geoserver Rest
     '''
     geo.create_featurestore(store_name='gisweb', workspace='gisweb', db='geoapp', host='localhost', pg_user='bilalbenzer',
                         pg_password='gazi1453', schema = 'data'),
-    geo.publish_featurestore(workspace='gisweb', store_name='gisweb', pg_table=file_name)
+    geo.publish_featurestore(workspace='gisweb', store_name='gisweb', pg_table=name)
 
 
 
 @receiver(post_delete,sender=Shp)
-def delete_data(sender,instance,created,**kwargs):
-    pass
+def delete_data(sender,instance,**kwargs):
+    db.delete_table(table_name=instance.name, schema="public")
+    geo.delete_layer(instance.name,"geoapp")
 
     
