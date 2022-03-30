9c88b6ef3af01beb3f475ac5edad83979fe87b0e
Bilal Benzer <bllbnzr@gmail.com>
2022-03-30 16:19:06 +0300

    nokta oluşturma eklendi..

---
+++ 3DM_WEBSITE/_3dm_web_gis/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for _3dm_web_gis project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_3dm_web_gis.settings')
+
+application = get_wsgi_application()
