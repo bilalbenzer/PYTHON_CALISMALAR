06937eb6cfd8c0da860b581c88647d0720754c65
Bilal Benzer <bllbnzr@gmail.com>
2022-06-07 17:15:41 +0300

    geliştirmeler devam (multi point)

--- HTML-CSS-JS/js/create_multi_point.js
+++ HTML-CSS-JS/js/create_multi_point.js
@@ -5,37 +5,42 @@ function multi_create_point(){
     //Sayfa Mesajlarında Çizime Başlamaya Dair Bildiri
     document.getElementById('sayfamesajlari').innerText="Çizime Başlayabilirsiniz.\nHome: Koordinat Gir\nEnd: Bitir";
     document.getElementById('sayfamesajlari').style.backgroundColor  = "black";
+    sureli_bekletme(2000)
     var name = "multipoint"+(new Date()).getMilliseconds()+Math.floor(Math.random()*1000);//benzersiz id alma
+    if ((typeof window[name])!=="object"){
+        document.getElementById('sayfamesajlari').style.backgroundColor  = "black";
+        document.getElementById('sayfamesajlari').innerText="Çoklu Point Katmanı oluşturuldu\n"+name;//sayfa mesajlarında objenin oluştuğuna dair bilgi
+      }
     //harita üzerinde tıklama olayı ile koordinat almanın etkinleştirilmesi
     map.on('click', (e)=>{
     x = (e.latlng.lat).toFixed(8);
     y = (e.latlng.lng).toFixed(8);
     // oluşturulacak point için benzersiz bir id üretilir ve daha önceden bu id verilmiş mi kontrol edilir
-    var as;
-    for (as in tekdongu ){ 
       if ((typeof window[name])!=="object"){  //id in daha önceden var olup olmadığının kontrolü
-          document.getElementById("vektor").open = true
+          
+          document.getElementById("vektor").open = true //katman penceresi sürekli açık olacak
           document.getElementById('sayfamesajlari').style.backgroundColor  = "black"; 
           var name2 = "point"+(new Date()).getMilliseconds()+Math.floor(Math.random()*1000);//benzersiz obje id alma
-          window[name]= new multi_point(x_coordinats=x,y_coordinats=y,object_name=name2); //objenin oluşturulması
-          window[name].point_ekle(window[name].object_name)
+          window[name]= new multi_point(x_coordinats=x,y_coordinats=y,object_name=name2,class_name=name); //alınan koordinat ve alınan rastgele obje adına göre çoklu point sınıfı oluşturulur
           document.getElementById('sayfamesajlari').innerText="Point oluşturuldu \n"+"E="+x+"   "+"B="+y;//sayfa mesajlarında objenin oluştuğuna dair bilgi
-          var a = window[name].object_id_list.includes(name2)
-          console.log(a)
+          window[name].menuleriolustur()
+          window[name].haritayaekle(window[name].geojsonfutures)
         }
       else if ((typeof window[name])=="object" && (window[name].geojsonfutures.length > 0)) {
         document.getElementById("vektor").open = true
         document.getElementById('sayfamesajlari').style.backgroundColor  = "black"; 
         var name2 = "point"+(new Date()).getMilliseconds()+Math.floor(Math.random()*1000);//benzersiz obje id alma
         if (window[name].object_id_list.includes(name2)===true){
-          alert("tekrar deneyiniz")}
+            alert("tekrar deneyiniz")}
         else{
-          window[name].point_ekle(x_coordinats,y_coordinats,object_name=name2)
+            window[name].point_ekle(x_coordinats=x,y_coordinats=y,object_name=name2)
+            window[name].haritayaekle(window[name].geojsonfutures)
+            console.log(window[name].geojsonfutures)
         }
-      }
+        }
       else{
         alert("Bu İd'ye Sahip Bir Obje Bulunmakta.");   
-           }}});
+           }});
      // klavye kısayollarının etkinleştirilmesi
     document.addEventListener('keydown', function abc(event)  {
     var code = event.code;
@@ -45,6 +50,7 @@ function multi_create_point(){
     document.getElementById('obje_girdi').innerHTML='<label for="xbuton">E :</label><input type="number" maxlength="999999" id="xbuton" step="0.001" value="" required><br><label for="ybuton">B :</label><input type="number" maxlength="999999" id="ybuton" step="0.001" value="" required><input id="koordinatal" type="submit"  value="nokta oluştur">';
     document.getElementById("koordinatal").setAttribute("onclick","koordinatileolustur('"+"olustur"+"')")
     document.removeEventListener("keydown",abc)
+    map.off('click');
     }
     // end tuşuna basılması durumunda nokta oluşturma işlemi tamamlanır
     else if (code==="End") {
@@ -52,13 +58,14 @@ function multi_create_point(){
     document.getElementById('sayfamesajlari').innerText = "İşlem Tamamlandı";
     document.removeEventListener("keydown",abc)
     bekleme();
+    map.off('click');
     }
     });
   }
 
   class multi_point {
-    constructor(x_coordinats,y_coordinats,object_name){
-        var random_name = object_name+"merhaba"
+    constructor(x_coordinats,y_coordinats,object_name,class_name){
+        var random_name = object_name+".."  //obje ad
         this.random_name = {     //--->objenin öznitelik bilgilerinin tutulduğu nesne
             "featureid":object_name,
             "Geometri Tipi":"Nokta",
@@ -72,14 +79,39 @@ function multi_create_point(){
             "geometry":{
               "type":"Point",
               "coordinates":[y_coordinats,x_coordinats]}}
-        
+        this.id_nosu = class_name 
         this.geojsonfutures=[this.object_name]
         this.object_id_list = [object_name]
-        console.log(this.object_id_list)
+        this.object_stil = {object_name:{bicim:{radius: 8,
+          fillColor: renk_listesi[Math.floor(Math.random()*renk_listesi.length)], //her oluşan obje bu algoritma ile rastgele bir renk alır
+          color: "#000",  //dış çizginin renki
+          weight: 1,  //dış çizginin kalınlığı
+          opacity: 3, //dış çizginin opaklığı
+          fillOpacity: 0.8},
+          ikon:{ //objenin sembol ayarları bu nesneye gider
+            iconUrl:null,
+            iconSize:[50,50],
+          },
+          gecerli_isaret:"nokta",
+          id:object_name}}
+        this.objeler_ozellikleri=[
+                this.object_stil]
+        console.log(this.objeler_ozellikleri)
         console.log(this.geojsonfutures)
     }
     point_ekle(x_coordinats,y_coordinats,object_name){
+      var stil = {object_name:{bicim:{radius: 8,
+        fillColor: renk_listesi[Math.floor(Math.random()*renk_listesi.length)], //her oluşan obje bu algoritma ile rastgele bir renk alır
+        color: "#000",  //dış çizginin renki
+        weight: 1,  //dış çizginin kalınlığı
+        opacity: 3, //dış çizginin opaklığı
+        fillOpacity: 0.8},
+        ikon:{ //objenin sembol ayarları bu nesneye gider
+          iconUrl:null,
+          iconSize:[50,50],
+        }}}
       this.object_id_list.push(object_name)
+      this.objeler_ozellikleri.push(stil)
       this.geojsonfutures.push({
         "type":"Feature",
         "properties":{"featureid":object_name,
@@ -88,8 +120,44 @@ function multi_create_point(){
                       "Y Koordinatı(Boylam)":parseFloat(y_coordinats)},
         "geometry":{
             "type":"Point",
-            "coordinates":[y_coordinats,x_coordinats]        }
+            "coordinates":[y_coordinats,x_coordinats]}
       })
     }
-    
+    menuleriolustur(){
+      this.details_katman =document.createElement("details");
+      this.details_katman.setAttribute("id",this.id_nosu);
+      this.details_katman.setAttribute("name",this.id_nosu);
+      this.summary_katman = document.createElement("summary");
+      this.summary_katman.setAttribute("id",this.id_nosu+"_summary");
+      document.getElementById("layers_vektor").appendChild(this.details_katman);
+      document.getElementById(this.id_nosu).innerHTML='<button class="haritadagosterme" type="menu" >Haritada Göster</button><button class="haritadagizleme" type="menu" >Haritada Gizle</button><button class="sil" type="menu">Katmanı Sil</button><button class="duzenle" type="menu">Katmanı Düzenle</button><button class="yaklasma" type="menu" >Yaklaş</button><button class="stildegistir" type="menu" >Stil Değiştir</button><button class="oznitelikbilgi" type="menu" >Öznitelikleri Görüntüle ve Düzenle</button>';
+      document.getElementById(this.id_nosu).appendChild(this.summary_katman);
+      document.getElementById(this.id_nosu+"_summary").innerText=this.id_nosu;
+      document.getElementById(this.id_nosu+"_summary").style.listStyle="none";
+      document.getElementById(this.id_nosu+"_summary").style.fontSize="medium";
+      document.getElementById(this.id_nosu+"_summary").style.fontWeight="bolder";
+      document.getElementById(this.id_nosu+"_summary").style.color="black";
+      document.getElementById(this.id_nosu+"_summary").style.fontFamily="'Courier New', Courier, monospace";
+      document.getElementById(this.id_nosu+"_summary").style.cursor="pointer";
+      document.getElementById(this.id_nosu+"_summary").style.listStyle="none";
+      document.getElementById(this.id_nosu+"_summary").style.borderStyle="solid";
+      document.getElementById(this.id_nosu+"_summary").style.backgroundColor=renk_listesi[Math.floor(Math.random()*renk_listesi.length)];
+      document.getElementById(this.id_nosu+"_summary").style.borderColor="white";
+      //butonlar ve kullanılacakları işlevler
+      
+      document.getElementById(this.id_nosu).getElementsByTagName("button")[0].setAttribute('onclick',"window['"+this.id_nosu+"'].objeyiyenile(window['"+this.id_nosu+"'])");
+      document.getElementById(this.id_nosu).getElementsByTagName("button")[1].setAttribute('onclick',"window['"+this.id_nosu+"'].haritadagizle()");
+      document.getElementById(this.id_nosu).getElementsByTagName("button")[2].setAttribute('onclick',"katman_sil('"+this.id_nosu+"')");
+      document.getElementById(this.id_nosu).getElementsByTagName("button")[3].setAttribute('onclick',"window['"+this.id_nosu+"'].katmanduzenle(window['"+this.id_nosu+"'])");
+      document.getElementById(this.id_nosu).getElementsByTagName("button")[4].setAttribute('onclick',"window['"+this.id_nosu+"'].objeyeyaklas()");
+      document.getElementById(this.id_nosu).getElementsByTagName("button")[5].setAttribute('onclick',"window['"+this.id_nosu+"'].stildegistirme()");
+      document.getElementById(this.id_nosu).getElementsByTagName("button")[6].setAttribute('onclick',"window['"+this.id_nosu+"'].oznitelikgoruntulemeveduzenleme()");
+    }
+    haritayaekle(x){
+      this.layer = L.geoJSON(this.geojsonfutures,{
+        pointToLayer:function(feature,latlng){
+          return L.circleMarker(latlng,x)
+        }
+      }).addTo(map)
+    }
   }
